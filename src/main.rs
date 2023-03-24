d705dec4c33a9e5a07c29cbea931534b87d2cc25
Eduardo Padoan <eduardo.padoan@getbyrd.com>
2023-03-24 11:59:08 -0300

    Handle subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,36 @@
 #![allow(dead_code, unused_variables, unused_imports)]
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use goals::Goal;
-use rusqlite::{params, Connection, Result};
+use rusqlite::Result;
 
-#[derive(Parser)] // requires `derive` feature
-#[command(name = "goals")]
-#[command(bin_name = "goals")]
-enum GoalsCli {
-    Weekly(WeeklyGoalArgs),
+#[derive(Subcommand)]
+enum GoalsCommands {
+    Weekly,
+    Daily,
+    ListTasks,
 }
 
-#[derive(clap::Args, Debug)]
-#[command(author, version, about, long_about = "List/Set your weekly goal(s)")]
-struct WeeklyGoalArgs {
-    #[arg(long)]
-    text: Option<String>,
+#[derive(Parser)]
+#[command(name = "goals")]
+#[command(bin_name = "goals")]
+struct GoalsCli {
+    #[command(subcommand)]
+    command: Option<GoalsCommands>,
 }
 
 fn main() -> Result<()> {
-    let GoalsCli::Weekly(args) = GoalsCli::parse();
-    let goal = Goal::input();
+    let cli = GoalsCli::parse();
 
-    print!("{:?}", args);
-    println!("Weekly goal: {:?}", goal);
+    let goal: Option<Goal> = match &cli.command {
+        Some(GoalsCommands::Weekly) => Some(Goal::input()),
+        Some(GoalsCommands::Daily) => Some(Goal::input()), // TODO...
+        Some(GoalsCommands::ListTasks) => Some(Goal::input()), // TODO...
+        None => None,
+    };
 
-    goal.save()
+    if let Some(goal) = goal {
+        goal.save()
+    } else {
+        Ok(())
+    }
 }
